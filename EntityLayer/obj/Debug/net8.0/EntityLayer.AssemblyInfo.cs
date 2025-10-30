622f285cb6b58f35767bc061f6d13be538ce4696
caglakucuk <caglakucuk39@gmail.com>
2025-10-31 01:25:35 +0300

    Remove .DS_Store and ignore it

--- EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
+++ EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bccd52e85168dcb0fe36922af2ab6eb6e03c96c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44f127d94c4705ea7bc5da6b444e02376c5726d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
